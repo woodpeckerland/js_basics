4941aa005306e60b1f08076d37edc9b594c01ef3
woodpecker <hans.peter.specht@gmail.com>
2025-06-18 12:59:06 +0200

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log('Hello World');
+
+// variables
+
+// cannot be a reserved keyword
+// should be meaningful
+// cannot start with a number
+// cannot contain a space or hyphen
+// are case-sensitive
+
+let firstName = 'Peter';
+let lastName = 'Specht';
+
+// constants
+const interestRate = 0.3;
+console.log(interestRate);
+
+// primitives, value types
+let name = 'Peter'; // String literal
+let age = 45; // Number literal
+let isApproved = true; // Boolean Literal
+let firstNameTypes = undefined;
+let selectedColor = null;
+
+// 
